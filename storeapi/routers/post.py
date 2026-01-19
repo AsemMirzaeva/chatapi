0802e8191c9329559303e0f84f217d28d41773dd
AsemMirzaeva <asemmirzaeva1@gmail.com>
2026-01-19 21:35:58 +0500

    new features are written

---
+++ storeapi/routers/post.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter,HTTPException
+
+from storeapi.models.post import UserPost, UserPostIn, Comment, CommentIn
+
+
+router = APIRouter()
+post_table = {}
+comment_table = {}
+
+
+def find_post(post_id: int):
+    return post_table.get(post_id)
+
+
+
+@router.post("/post", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+@router.get("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
+
+@router.post("/comment", response_model=Comment)
+async def create_comment(comment: CommentIn):
+    
+    data = comment.dict()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "id": last_record_id}
+    comment_table[last_record_id] = new_comment
+    return new_comment
