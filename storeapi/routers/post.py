aa6120b83dd675eac8ce789ec5837d2b783febf5
AsemMirzaeva <asemmirzaeva1@gmail.com>
2026-04-30 17:22:31 +0500

    loggers were added

--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -1,14 +1,20 @@
+import logging
+
 from fastapi import APIRouter, HTTPException
 from storeapi.database import comment_table, post_table, database
 from storeapi.models.post import UserPost, UserPostIn, Comment, CommentIn, UserPostWithComments
 
 router = APIRouter()
 
-
+logger = logging.getLogger(__name__)
 
 
 async def find_post(post_id: int):
+    logger.info(f"Finding post with id {post_id}")
     query = post_table.select().where(post_table.c.id == post_id)
+
+    logger.debug(query)
+
     return await database.fetch_one(query)
 
 @router.post("/post", response_model=UserPost, status_code=201)
@@ -21,10 +27,15 @@ async def create_post(post:UserPostIn):
 @router.get("/post", response_model=list[UserPost])
 async def get_all_posts():
     query = post_table.select()
+
+    query = post_table.select()
+    logger.debug(query)
+
     return await database.fetch_all(query)
 
 @router.post("/comment", response_model=Comment, status_code=201)
 async def create_comment(comment:CommentIn):
+    logger.info("Creating comment")
     post = await find_post(comment.post_id)
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
@@ -36,12 +47,19 @@ async def create_comment(comment:CommentIn):
 
 @router.get("/post/{post_id}/comment", response_model=list[Comment])
 async def get_comments_on_post(post_id: int):
+    logger.info("Getting comments on posts")
+
     query = comment_table.select().where(comment_table.c.post_id == post_id)
+
+    logger.debug(query)
+
     return await database.fetch_all(query)
 
 
 @router.get("/post/{post_id}", response_model=UserPostWithComments)
 async def get_post_with_comments(post_id: int):
+    logger.info("Getting post and its comments")
+
     post = await find_post(post_id)
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
